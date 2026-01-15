01a628f231cdf6d6e682a145e2fada83ec7569ff
MarwanePRO <marwane.charfa@gmail.com>
2026-01-15 10:34:53 +0100

    Correct import statements in DemoGithubApplication
    Fix import statements for Spring framework.

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -2,6 +2,8 @@ package com.example.demo_github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.spirngframework.web.bind.annotation.GetMapping;
+import org.spirngframework.web.bind.annotationRestController;
 
 @SpringBootApplication
 public class DemoGithubApplication {
